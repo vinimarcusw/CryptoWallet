deaec93a58aaeaddc80b64941bedfdb33a89903c
vinimarcusw <vinicius.bressanini@warren.com.br>
2022-09-19 16:18:32 -0300

    feat: Creating graphic

--- lib/details/widgets/details_page_body.dart
+++ lib/details/widgets/details_page_body.dart
@@ -1,4 +1,5 @@
 import 'package:crypto_wallet/details/widgets/crypto_variation_info.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -9,6 +10,7 @@ import 'crypto_prices_information.dart';
 import 'crypto_fraction_bottom_info.dart';
 import 'crypto_top_list_tile.dart';
 import 'crypto_total_value_row.dart';
+import 'middle_line_chart.dart';
 
 class DetailsPageBody extends HookConsumerWidget {
   const DetailsPageBody({
@@ -18,26 +20,29 @@ class DetailsPageBody extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final asset = ref.watch(assetProvider);
-    return Column(
-      children: [
-        const CryptoTopListTile(),
-        const CryptoTotalValueRow(),
-        const SizedBox(height: 190),
-        const Divider(thickness: 1),
-        CryptoPricesInformation(
-            value: asset.cryptoTotal,
-            title: AppLocalizations.of(context)!.detailsActualPrice),
-        const Divider(thickness: 1),
-        const CryptoVariationInfo(),
-        const Divider(thickness: 1),
-        const CryptoFractionBottomInfo(),
-        const Divider(thickness: 1),
-        CryptoPricesInformation(
-            value: asset.cryptoValues.last,
-            title: AppLocalizations.of(context)!.detailsValue),
-        const SizedBox(height: 20),
-        const BottomConvertButton(),
-      ],
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          const CryptoTopListTile(),
+          const CryptoTotalValueRow(),
+          const MiddleLineChart(),
+          const Divider(thickness: 1),
+          CryptoPricesInformation(
+              value: asset.cryptoTotal,
+              title: AppLocalizations.of(context)!.detailsActualPrice),
+          const Divider(thickness: 1),
+          const CryptoVariationInfo(),
+          const Divider(thickness: 1),
+          const CryptoFractionBottomInfo(),
+          const Divider(thickness: 1),
+          CryptoPricesInformation(
+              value: asset.cryptoValues.last,
+              title: AppLocalizations.of(context)!.detailsValue),
+          const SizedBox(height: 20),
+          const BottomConvertButton(),
+        ],
+      ),
     );
   }
 }
